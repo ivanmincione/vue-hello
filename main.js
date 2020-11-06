eff9604602f600caf79030bc02f8e83a726a7fac
Ivan Mincione <ivan.mincione92@outlook.it>
2020-11-06 16:46:54 +0100

    Add title whit Vue.js

---
+++ main.js
@@ -0,0 +1,13 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+// BONUS: Aggiungere un’immagine presa anch’essa da un data
+
+
+// richiamo Vue.Js
+var app = new Vue({
+    el: "#root", // per convenzione il contenitore generale in cui lavora Vue viene indicato con il nome root
+    data: { // contenitore dei dati necessari
+        message: "Hello Vue!",
+        
+    }
+});

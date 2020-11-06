9677aaecdba378d5289ecdcc43ed7c38bf97dbfd
Ivan Mincione <ivan.mincione92@outlook.it>
2020-11-06 17:41:38 +0100

    Add img whit Vue.js and simple button change mode

--- main.js
+++ main.js
@@ -8,6 +8,16 @@ var app = new Vue({
     el: "#root", // per convenzione il contenitore generale in cui lavora Vue viene indicato con il nome root
     data: { // contenitore dei dati necessari
         message: "Hello Vue!",
-        
+        title: "style-title",
+        image: " https://d585tldpucybw.cloudfront.net/sfimages/default-source/default-album/vuel_870x220bf59eb56a270429dafdbc9ae3513253c.png?sfvrsn=49fafa3a_1 ",
+    },
+    methods: {
+        change() {
+            if (this.title == "style-title") {
+                this.title = "style-title-dark root-dark ";
+            } else {
+                this.title = "style-title";
+            }
+        }
     }
 });
